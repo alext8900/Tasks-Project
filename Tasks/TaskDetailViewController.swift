ca6a62c20ce2067dd3353c1b5f9b21983fb97852
Alex Thompson <guitarman226@gmail.com>
2019-12-03 21:07:13 -0600

    Added Data model, created UI, added code files to goto things in the storyboard :)

---
+++ Tasks/TaskDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaskDetailViewController.swift
+//  Tasks
+//
+//  Created by Alex Thompson on 12/3/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
